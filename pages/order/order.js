76170b8b08d12e1623bc81762aff430ea93d1d31
zhangping2 <zhangping2@yy.com>
2026-03-12 11:34:33 +0800

    加购

--- pages/order/order.js
+++ pages/order/order.js
@@ -3,7 +3,9 @@ const app = getApp();
 
 Page({
   data: {
-    products: [] // 商品列表
+    products: [], // 商品列表
+    totalPrice: 0, // 总价
+    cartVisible: false // 购物车半窗是否显示
   },
 
   onLoad: function (options) {
@@ -45,6 +47,71 @@ Page({
     products[index].count += 1;
     this.setData({
       products: products
+    });
+    this.calculateTotalPrice();
+  },
+
+  // 减少商品数量
+  minusCount: function (e) {
+    const index = e.currentTarget.dataset.index;
+    const products = this.data.products;
+    if (products[index].count > 0) {
+      products[index].count -= 1;
+      this.setData({
+        products: products
+      });
+      this.calculateTotalPrice();
+    }
+  },
+
+  // 计算总价
+  calculateTotalPrice: function () {
+    const products = this.data.products;
+    let total = 0;
+    products.forEach(item => {
+      total += item.price * item.count;
+    });
+    this.setData({
+      totalPrice: total
+    });
+  },
+
+  // 点击购物车，显示半窗
+  toggleCart: function () {
+    this.setData({
+      cartVisible: !this.data.cartVisible
+    });
+  },
+
+  // 购物车中增加数量
+  cartAddCount: function (e) {
+    const index = e.currentTarget.dataset.index;
+    const products = this.data.products;
+    products[index].count += 1;
+    this.setData({
+      products: products
+    });
+    this.calculateTotalPrice();
+  },
+
+  // 购物车中减少数量
+  cartMinusCount: function (e) {
+    const index = e.currentTarget.dataset.index;
+    const products = this.data.products;
+    if (products[index].count > 0) {
+      products[index].count -= 1;
+      this.setData({
+        products: products
+      });
+      this.calculateTotalPrice();
+    }
+  },
+
+  // 去结算
+  goToSettle: function () {
+    wx.showToast({
+      title: '去结算待实现',
+      icon: 'none'
     });
   }
 });
